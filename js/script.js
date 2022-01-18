18c876ba0652dc83da60339ff669bf7e66ed2cf9
Sumia <87886688+Sumia93@users.noreply.github.com>
2022-01-18 15:07:22 +0200

    task

---
+++ js/script.js
@@ -0,0 +1,75 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleID, navId) => {
+    const toggle = document.getElementById(toggleID),
+        nav = document.getElementById(navId);
+    if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+            nav.classList.toggle("show-menu");
+        });
+    }
+}
+showMenu('nav_toggle', 'nav_menu');
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+    const navMenu = document.getElementById('nav_menu');
+    navMenu.classList.remove("show-menu");
+}
+navLink.forEach(n => n.addEventListener("click", linkAction));
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive)
+    /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const nav = document.querySelector('header');
+    if (this.scrollY >= 200) nav.classList.add('scroll-header');
+    else nav.classList.remove('scrollheader');
+}
+window.addEventListener('scroll', scrollHeader);
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+    const $scrollTop = document.querySelector('scrollTop');
+    if (this.scrollY >= 200) $scrollTop.classList.add('show-scroll');
+    else $scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = "dark-theme";
+const iconTheme = 'bx-sun';
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = document.body.classList.contains(darkTheme) ? 'dark' : "light";
+const getCurrentIcon = document.body.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+});
